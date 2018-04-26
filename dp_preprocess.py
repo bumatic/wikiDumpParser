7726ba8dbc2e419e44ad9efa1a610f498c587460
Marcus Burkhardt <marcus.burkhardt@gmail.com>
2018-04-26 22:32:29 +0200

    error handling

--- dp_preprocess.py
+++ dp_preprocess.py
@@ -2,6 +2,7 @@ from wikiDumpParser.wikiDumpParser import *
 
 project = Project()
 project.load_project()
+project.set_parallel_processes(1)
 project.get_templates()
 #project.update_status()
 #project.retry_errors()
